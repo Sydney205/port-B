f77e80714190a45ed92e3df5ec847c9ffa9a3ed9
Dieke Sydney <diekechi@gmail.com>
2023-01-01 12:36:22 +0000

    deleted

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3500;
-
-app.listen(PORT, () => {
-  console.log(`Server running on PORT ${PORT}`);
-});
